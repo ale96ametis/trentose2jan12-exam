30bc8c6c2714340cec474d100aa4ea8e16bed7e1
Alessandro Ametis <alessandro.ametis@gmail.com>
2018-01-12 09:54:03 +0100

    simple get and post new astronut

---
+++ astronauts.js
@@ -0,0 +1,28 @@
+const express = require('express'),
+    bodyParser = require('body-parser');
+const astronauts = express.Router()
+
+var uuid = require('uuid-v4');
+
+const astronautsArray = []
+
+astronauts.route('/astronauts')
+  //simpleget return all objects
+    .get((req, res) => {
+    res.status(200)
+    res.json(astronautsArray)
+  })
+//post an object
+  .post((req, res) => {
+    var astronaut = {}
+    astronaut.id = uuid()
+    if (req.body.firstName) astronaut.firstName = req.body.firstName
+    if (req.body.lastName) astronaut.lastName = req.body.lastName
+    if (req.body.isInSpace) astronaut.isInSpace = req.body.isInSpace
+    astronauts.push(astronaut)
+    res.status(200)
+    res.json(astronaut)
+  });
+
+
+module.exports = astronauts
